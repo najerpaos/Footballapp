ba214551948d8876b26f3b06cb71965f8d9f6b2e
Sergio s <naerjpaos@gmail.com>
2023-05-15 00:48:35 +0200

    version 2 with Firebase

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Homescreen=()=>{
+    return <table class="table table-bordered border-dark table-striped table-hover">
+      <thead class="table-success">
+        <tr>
+          <th>Date</th>
+          <th>Event</th>
+          </tr>
+      </thead>
+      <tbody>
+          <tr>
+              <td>August 4</td>
+              <td>NYSL Fundraiser</td>
+          </tr>
+          <tr>
+              <td>August 16</td>
+              <td>Season Kick-off: Meet the Teams</td>
+          </tr>
+          <tr>
+              <td>September 1</td>
+              <td>First Game of the Season (Check Game Schedule for details)</td>
+          </tr>
+      </tbody>
+  </table>
+}
+
+export default Homescreen
